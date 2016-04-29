9249984ba5acb4fc0c7f5db3e40ffc18040ca713
FuadMunzerinHossain <fuad.hossain@mail.utoronto.ca>
2016-04-28 21:14:06 -0400

    added more functionality by allowing to rename and delete files and directories, as well as minor touchups to other functions

--- python-ftp-client.py
+++ python-ftp-client.py
@@ -10,9 +10,12 @@ import ftplib
 
 def print_dir():
     """ function to print out files and directory names in current FTP server directory """
-    print('File List:')
-    files = ftp.dir()
-    print(files)
+    try:
+        print('File List:')
+        files = ftp.dir()
+        print(files)
+    except Exception as e:
+        print('Exception: ' + str(e))
     return
 
 
@@ -20,8 +23,8 @@ def change_dir(argument):
     """ function to change directory on FTP server"""
     try:
         ftp.cwd(argument)
-        print_dir()
-    except:
+    except Exception as e:
+        print('Exception: ' + str(e))
         print('Error: Failed to change directory!')
     return
 
@@ -33,26 +36,64 @@ def download_file(name):
         ftp.retrbinary('RETR ' + name, f.write)
         f.close()
         print(name + ' has been downloaded to local directory.')
-    except:
+    except Exception as e:
+        print('Exception: ' + str(e))
         print('Error: File does not exist or could not be downloaded!')
+    return
+
 
 def mkdir(name):
     """ function to make new directory on FTP server """
     try:
         ftp.mkd(name)
         print('Directory ' + name + ' successfully created.')
-    except ftplib.error_perm as e:
-        print(e)
+    except Exception as e:
+        print('Exception: ' + str(e))
         print('Directory ' + name + ' could not be created!')
+    return
+
+
+def rename(old_name, new_name):
+    """ function to rename file/directory on FTP server """
+    try:
+        ftp.rename(old_name, new_name)
+        print(old_name + ' successfully renamed to ' + new_name + '.')
+    except Exception as e:
+        print('Exception: ' + str(e))
+        print('Could not rename ' + old_name + ' to ' + new_name + '!')
+    return
+
+
+def delete_file(name):
+    """ function to delete file on FTP server """
+    try:
+        ftp.delete(name)
+        print('Successfully deleted ' + name + '.')
+    except Exception as e:
+        print('Exception: ' + str(e))
+        print('Could not delete ' + name + '!')
+    return
+
+
+def delete_directory(name):
+    """ function to delete directory on FTP server """
+    try:
+        ftp.rmd(name)
+        print('Successfully deleted ' + name + '.')
+    except Exception as e:
+        print('Exception: ' + str(e))
+        print('Could not delete ' + name + '!')
+    return
+
 
 ftp = ftplib.FTP()  # initiate FTP object
-# default settings
+""" default settings """
 ip_address = '192.168.0.17'  # replace with default FTP IP Address
 port = 3721  # replace with default FTP port (usually 21, but can be different)
 username = 'Fuad123'  # replace with default FTP username
 password = 'voldemort'  # replace with default FTP password
 
-cmd = input('Use default settings? (y/n): ')
+cmd = input('Use default settings? (y/n) (select n if you did not change source file): ')
 if cmd == 'n' or cmd == 'N':
     ip_address = input('Enter IP Address: ')
     port = int(input('Enter port: '))
@@ -65,9 +106,10 @@ ftp.login(username, password)
 print(ftp.getwelcome())
 
 while True:
-    cmd = input('-> ')
+    # cmd = input('-> ')
+    cmd = input('root' + ftp.pwd() + '> ')
     cmd_list = cmd.split()
-    cmd = cmd_list.pop(0)
+    cmd = cmd_list.pop(0).lower()
     cmd_args = " ".join(cmd_list)
 
     if cmd == 'dir' or cmd == 'ls':
@@ -76,11 +118,46 @@ while True:
     elif cmd == 'cwd' or cmd == 'cd':
         change_dir(cmd_args)
 
-    elif cmd == 'download' or cmd == 'retr' or cmd == 'RETR':
+    elif cmd == 'download' or cmd == 'retr':
         download_file(cmd_args)
 
     elif cmd == 'mkd' or cmd == 'mkdir':
         mkdir(cmd_args)
+
+    elif cmd == 'rename' or cmd == 'mv':
+        try:
+            old_name = cmd_list.pop(0)
+            new_name = cmd_list.pop(0)
+            prompt = input('Do you want to change ' + old_name + ' to ' + new_name + '? (y,n,m)\n' +
+                           'Select m to type the names separately if the names have spaces: ').lower()
+            if prompt == 'y':
+                rename(old_name, new_name)
+            elif prompt == 'm':
+                old_name = input('Which file/directory do you want to rename?')
+                new_name = input('What do you want to rename ' + old_name + ' to?')
+                rename(old_name, new_name)
+        except Exception as e:
+            print('Exception: ' + str(e))
+            print('Invalid arguments')
+
+    elif cmd == 'rm':
+        confirm = input('Are you sure you want to delete file ' + cmd_args + '?(y/n): ').lower()
+        if confirm == 'y':
+            delete_file(cmd_args)
+        else:
+            print('Not deleting ' + cmd_args + ".")
+
+    elif cmd == 'rmd':
+        confirm = input('Are you sure you want to delete directory ' + cmd_args + '?(y/n): ').lower()
+        if confirm == 'y':
+            delete_directory(cmd_args)
+        else:
+            print('Not deleting ' + cmd_args + ".")
+
+    elif cmd == 'quit':
+        break
+
+
 
 
 
